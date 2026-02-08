39e484a57e4f44ab214863bf4919b633b7bdeffb
Said Chahir <schahir@c2r9p10.1337.ma>
2026-02-08 17:49:28 +0100

    s

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,18 +6,18 @@
 /*   By: schahir <schahir@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 05:09:01 by schahir           #+#    #+#             */
-/*   Updated: 2026/02/08 05:09:50 by schahir          ###   ########.fr       */
+/*   Updated: 2026/02/08 17:49:15 by schahir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    if (argc != 2)
+    if (ac != 2)
     {
         std::cout << "Invalid arguments" << std::endl;
         return 1;
     }
-    ScalarConverter::convert(argv[1]);
+    ScalarConverter::convert(av[1]);
 }

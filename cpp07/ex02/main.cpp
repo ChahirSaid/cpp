067069cb5bb941c97a3fa02aef34f8b492956fe2
Said Chahir <schahir@c2r3p1.1337.ma>
2026-05-07 14:52:02 +0100

    s

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: schahir <schahir@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 10:22:01 by schahir           #+#    #+#             */
-/*   Updated: 2026/05/07 13:27:26 by schahir          ###   ########.fr       */
+/*   Updated: 2026/05/07 14:51:53 by schahir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #define MAX_VAL 750
 int main(int, char**)
 {
+    
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
@@ -23,6 +24,19 @@ int main(int, char**)
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
+    }
+    {
+        const Array<int>& const_ref = numbers;
+        std::cout << "First element via const ref: " << const_ref[0] << std::endl;
+        
+        try
+        {
+            std::cout << const_ref[MAX_VAL] << std::endl;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
     }
     //SCOPE
     {

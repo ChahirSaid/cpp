2bc6ef556cb634c4ae4a528f35a09041acd0d528
Said Chahir <schahir@c2r2p3.1337.ma>
2026-05-19 21:43:47 +0100

    Span

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: schahir <schahir@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 17:19:26 by schahir           #+#    #+#             */
-/*   Updated: 2026/05/19 15:08:35 by schahir          ###   ########.fr       */
+/*   Updated: 2026/05/19 21:43:14 by schahir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,21 @@ int main()
     {
         std::cout << e.what() << std::endl;
     }
+    
+    std::vector<int> numbers;
+    for (int i = 0; i < 10; i++)
+    {
+        numbers.push_back(i);
+    }
     Span S = Span(20000);
     try
     {
-        // S.addMulti();
+        S.addMulti(numbers.begin(), numbers.end());
+        for (std::vector<int>::iterator it = numbers.begin(); it != numbers.end(); ++it)
+        {
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl;
         std::cout << S.shortestSpan() << std::endl;
         std::cout << S.longestSpan() << std::endl;
     }

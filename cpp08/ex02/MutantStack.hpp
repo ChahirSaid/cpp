e7ada047fb31a1206619217ec1292dd3be137f05
Said Chahir <schahir@c2r3p3.1337.ma>
2026-05-19 14:05:06 +0100

    s

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: schahir <schahir@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 14:51:20 by schahir           #+#    #+#             */
-/*   Updated: 2026/05/18 22:48:11 by schahir          ###   ########.fr       */
+/*   Updated: 2026/05/19 14:04:53 by schahir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,22 @@
 
 template <typename T>
 
-class MutantStack : public std::stack T
+class MutantStack : public std::stack <T>
 {
     public:
         typedef typename std::stack <T>::container_type::iterator               iterator;
         typedef typename std::stack <T>::container_type::const_iterator         const_iterator;
         typedef typename std::stack <T>::container_type::reverse_iterator       reverse_iterator;
         typedef typename std::stack <T>::container_type::const_reverse_iterator const_reverse_iterator;
-        
-        
+
+        iterator begin() {return this->c.begin();}
+        iterator end() {return this->c.end();}
+        const_iterator begin() const {return this->c.begin();}
+        const_iterator end() const {return this->c.end();}
+        reverse_iterator rbegin() {return this->c.rbegin();}
+        reverse_iterator rend() {return this->c.rend();}
+        const_reverse_iterator rbegin() const {return this->c.rbegin();}
+        const_reverse_iterator rend() const {return this->c.rend();}
 };
 
 #endif
